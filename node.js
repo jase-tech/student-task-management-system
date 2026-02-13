98b20b6ce1b6ebee039964f96c47eb39fe48f12f
jase-tech <103462247+jase-tech@users.noreply.github.com>
2026-02-13 04:06:42 -0800

    Add files via upload

---
+++ node.js
@@ -0,0 +1,12 @@
+import React from "react"
+const { useInsertionEffect } = require("react")
+
+console.log('Student Task Managemt Service')
+console.assert('Title'=String,'ID'=CharacterData, 'Description'=String)
+
+console.
+return(ID &'', Tittle &'',Description &'') 
+
+console.clear (ID= 'Tittle'& 'Description')
+
+useInsertionEffect
